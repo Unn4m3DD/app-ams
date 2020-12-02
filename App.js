240c05a9d471af60cbfbf69fc4556bb2d8c166b3
Andre Patacas <guerreiroapatacas@gmail.com>
2020-12-02 11:10:50 +0000

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Vet" component={() => <Text>VetScreen</Text>} />
+        <Tab.Screen name="PetSitter" component={() => <Text>PetSitterScreen</Text>} />
+        <Tab.Screen name="Find" component={() => <Text>FindScreen</Text>} />
+        <Tab.Screen name="Food" component={() => <Text>FoodScreen</Text>} />
+        <Tab.Screen name="Settings" component={() => <Text>SettingsScreen</Text>} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
